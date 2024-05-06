5562bca2e0ca575ab64593a0fde4bc7f346328ee
rautbiraj <np03cs4a220196@heraldcollege.edu.np>
2024-05-06 21:25:23 +0545

    added comment in code

--- backend/app.js
+++ backend/app.js
@@ -5,7 +5,8 @@ const app = express();
 app.get('/', (req, res) => {
     res.send('Hello from the Node.js backend!');
 });
-
+ 
+//port for backend
 const PORT = process.env.PORT || 3000; // Use the environment port or 3000 as default
 app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`);
