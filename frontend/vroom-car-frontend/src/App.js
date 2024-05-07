d8c7bfbb7c5a2b708d9ed5243e91f10fd9d18fc9
Liven Maharjan <np03cs4a220111@heraldcollege.edu.np>
2024-05-07 21:17:46 +0545

    homepage and carpage

--- frontend/vroom-car-frontend/src/App.js
+++ frontend/vroom-car-frontend/src/App.js
@@ -2,13 +2,16 @@
 import React from 'react';
 import { BrowserRouter, Routes, Route } from 'react-router-dom'; 
 import Login from './Login';
+import HomePage from './homepage';
+import CarPage from './cars';
 function App() {
 return (
 
 <BrowserRouter>
      <Routes>
      <Route path='/Login' element={<Login/>} />
-
+     <Route path='/' element={<HomePage/>}/>
+      <Route path='/cars' element={<CarPage/>}/>
       </Routes>
     </BrowserRouter>
   );
---
+++ frontend/vroom-car-frontend/src/homepage.js
@@ -0,0 +1,64 @@
+import React from 'react';
+import './homepage.css';
+import { Link } from 'react-router-dom';
+
+function HomePage() {
+  return (
+    <div className='homepage-body'>
+      <div className="full-page">
+        <header>
+          <div className="containerbox">
+            <nav>
+            <h1>Vroom Car Rental Services</h1>
+              <ul>
+                <li className='home'><a href="#HomePage"> Home</a></li>
+                <li><a href="#services">Services</a></li>
+                <li><Link to={"/cars"}>Cars</Link></li>
+                <li><Link to={"Contactus"}>Contact Us</Link></li>
+
+                <li className='si'> 
+                <Link to={"/login"}>Login</Link>                
+                  </li>
+                  </ul>
+            </nav>
+          </div>
+        </header>
+
+        <div className='container2'>
+        <div className='fontt'>
+          <p>Pick Your Date: </p>
+
+        </div>
+        </div>
+
+        <div className="body-content">
+          <div className="containerT">
+            <div className="date-wrapper">
+                <label htmlFor="time">Pick up date:</label>
+                <input type="date" id="time" className="date" />
+                </div>
+
+                <button className='confirm-button'>
+                  <Link to={"/cars"} className='log1'>Confirm</Link></button>
+
+                <div className='date-wrapper2'>
+                <label htmmlFor="time2">Drop date:</label>
+                <input type="date" id="time2" className="date2" />
+                </div>
+              </div>
+            </div>
+  
+
+        <footer>
+          <div className="container">
+            <p>&copy; 2024(VroomCar). All rights reserved.</p>
+          </div>
+        </footer>
+        </div>
+        </div>
+        
+        
+  );
+}
+
+export default HomePage;
