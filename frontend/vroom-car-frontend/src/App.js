56b640e0c438c455dbe17fb683839e422205fae5
susanshakya11 <np03cs4a220113@heraldcollege.edu.np>
2024-05-07 18:56:44 +0545

    Added signup page

--- frontend/vroom-car-frontend/src/App.js
+++ frontend/vroom-car-frontend/src/App.js
@@ -1,18 +1,17 @@
-
 import React from 'react';
-import { BrowserRouter, Routes, Route } from 'react-router-dom'; 
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import Login from './Login';
+import Signup from './Signup'; // Import the Signup component
+
 function App() {
-return (
-
-<BrowserRouter>
-     <Routes>
-     <Route path='/Login' element={<Login/>} />
-
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path='/login' element={<Login />} /> {/* Route for Login component */}
+        <Route path='/signup' element={<Signup />} /> {/* Route for Signup component */}
       </Routes>
     </BrowserRouter>
   );
+}
 
-  
- }
 export default App;
