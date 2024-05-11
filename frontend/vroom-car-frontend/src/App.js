a20e3a896ec0f6f6c0ef5c5bed32f05a33612a72
susanshakya11 <np03cs4a220113@heraldcollege.edu.np>
2024-05-11 16:18:38 +0545

    Merge susan into main

--- frontend/vroom-car-frontend/src/App.js
+++ frontend/vroom-car-frontend/src/App.js
@@ -1,20 +1,25 @@
-
 import React from 'react';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import Login from './Login';
 import Signup from './Signup'; // Import the Signup component
+import CarPage from './cars';
+import Contactus from './contact';
+import UserProfile from './userProfile';
 
 function App() {
-return (
-
-<BrowserRouter>
-     <Routes>
-     <Route path='/Login' element={<Login/>} />
-
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path='/login' element={<Login />} /> {/* Route for Login component */}
+        <Route path='/signup' element={<Signup />} /> {/* Route for Signup component */}
+        <Route path='/' element={<HomePage/>}/>
+      <Route path='/cars' element={<CarPage/>}/>
+      <Route path='/contact' element={<Contactus/>}/>
+      <Route path='/Profile' element={<UserProfile/>}/>
+        
       </Routes>
     </BrowserRouter>
   );
+}
 
-  
- }
 export default App;
